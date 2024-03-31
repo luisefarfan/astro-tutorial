c4ebb3fdbeedf7a2552fc121596adb8ff59ae073
LuisFarfan57 <luise.prado99@hotmail.com>
2024-03-30 20:28:04 -0600

    feat: preact integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-tutorial-three-cyan.vercel.app/',
+  integrations: [preact()],
 });
